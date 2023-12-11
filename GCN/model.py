8509bd0603d43f29ab1d4fc7c60493d35494deff
Varenya Pathak <varenya7777@gmail.com>
2023-12-12 02:04:11 +0530

    gcn

--- GCN/model.py
+++ GCN/model.py
@@ -1,9 +1,5 @@
 import torch
-<<<<<<< HEAD
-from torch_geometric.nn import global_mean_pool, global_max_pool, GraphNorm, GCNConv
-=======
 from torch_geometric.nn import GATConv, GATv2Conv, global_mean_pool, global_max_pool, GraphNorm, GCNConv
->>>>>>> e7ae4b2ce2c1b7b36249d5250c05ca5738212256
 from torch.nn import Module, BatchNorm1d, Linear, ReLU
 from torch.nn.functional import sigmoid
 class GCNModel(Module):
@@ -13,94 +9,54 @@ class GCNModel(Module):
         # Reactant 1
         self.x_layer1 = GCNConv(in_channels=dataset[0].x_s.size(1), out_channels=dataset[0].x_s.size(1)//2, dropout=0.4)
         
-<<<<<<< HEAD
-        self.relu1 = ReLU()  
-=======
         self.relu1=ReLU()  
->>>>>>> e7ae4b2ce2c1b7b36249d5250c05ca5738212256
 
         self.gn1 = GraphNorm(in_channels=(dataset[0].x_s.size(1)//2))
 
         self.x_layer2 = GCNConv(in_channels=(dataset[0].x_s.size(1)//2), out_channels=dataset[0].x_s.size(1)//4, dropout=0.4)
 
-<<<<<<< HEAD
-        self.relu2 = ReLU()
-=======
         self.relu2=ReLU()
->>>>>>> e7ae4b2ce2c1b7b36249d5250c05ca5738212256
 
         self.gn2 = GraphNorm(in_channels=dataset[0].x_s.size(1)//4)
 
         self.x_layer3 = GCNConv(in_channels=(
             dataset[0].x_s.size(1)//4), out_channels=dataset[0].x_s.size(1)//8, dropout=0.4)
 
-<<<<<<< HEAD
-        self.relu3 = ReLU()
-=======
         self.relu3= ReLU()
->>>>>>> e7ae4b2ce2c1b7b36249d5250c05ca5738212256
 
         self.gn3 = GraphNorm(in_channels=dataset[0].x_t.size(1)//8)
 
         # Reactant 2
         self.y_layer1 = GCNConv(in_channels=dataset[0].x_t.size(1), out_channels=dataset[0].x_t.size(1)//2, dropout=0.4)
 
-<<<<<<< HEAD
-        self.relu4 = ReLU()
-=======
         self.relu4=ReLU()
->>>>>>> e7ae4b2ce2c1b7b36249d5250c05ca5738212256
 
         self.gn4 = GraphNorm(in_channels=(dataset[0].x_t.size(1)//2))
 
         self.y_layer2 = GCNConv(in_channels=(dataset[0].x_t.size(1)//2), out_channels=dataset[0].x_t.size(1)//4, dropout=0.4)
 
-<<<<<<< HEAD
-        self.relu5 = ReLU()
-=======
         self.relu5=ReLU()
->>>>>>> e7ae4b2ce2c1b7b36249d5250c05ca5738212256
 
         self.gn5 = GraphNorm(in_channels=dataset[0].x_s.size(1)//4)
 
         self.y_layer3 = GCNConv(in_channels=(
             dataset[0].x_t.size(1)//4), out_channels=dataset[0].x_t.size(1)//8, dropout=0.4)
 
-<<<<<<< HEAD
-        self.relu6 = ReLU()
-=======
         self.relu6=ReLU()
->>>>>>> e7ae4b2ce2c1b7b36249d5250c05ca5738212256
 
         self.gn6 = GraphNorm(in_channels=dataset[0].x_t.size(1)//8)
 
         # Linear Layers
         self.linear1 = Linear(in_features=(dataset[0].x_t.size(
             1)//8), out_features=329)
-<<<<<<< HEAD
-        
-        self.relu7 = ReLU()     
-
         self.bn1 = BatchNorm1d(num_features=329)
 
         self.linear2 = Linear(in_features=329, out_features=658)
-
-        self.relu8 = ReLU()
-
-=======
-        self.bn1 = BatchNorm1d(num_features=329)
-
-        self.linear2 = Linear(in_features=329, out_features=658)
->>>>>>> e7ae4b2ce2c1b7b36249d5250c05ca5738212256
         self.bn2 = BatchNorm1d(num_features=658)
 
         self.linear3 = Linear(in_features=658, out_features=1317)
 
-<<<<<<< HEAD
-    def forward(self, x, xs_batch, xt_batch,device):
-=======
     def forward(self, x, xs_batch, xt_batch):
->>>>>>> e7ae4b2ce2c1b7b36249d5250c05ca5738212256
         # graph1
         x1 = self.x_layer1(x.x_s, x.edge_index_s)
         x1 = self.relu1(x1)
@@ -121,32 +77,12 @@ class GCNModel(Module):
         x5 =self.relu5(x5)
         x5 = self.gn5(x5)
         x6 = self.y_layer3(x5, x.edge_index_t)
-<<<<<<< HEAD
-        x6 = self.relu6(x6)
-=======
->>>>>>> e7ae4b2ce2c1b7b36249d5250c05ca5738212256
         x6 = self.gn6(x6)
         xt = global_mean_pool(x6, batch=xt_batch)
 
         # # Aggregate
         x = torch.add(xs, xt)
 
-<<<<<<< HEAD
-        #For GPU
-        #xs = xs.to(device)
-        #xt = xt.to(device)
-
-        # Classifier
-        x = self.linear1(xs)
-        x = self.relu7(x)
-        x = self.bn1(x)
-        x = self.linear2(x)
-        x = self.relu8(x)
-        x = self.bn2(x)
-        x = self.linear3(x)
-
-        return x, sigmoid(x)
-=======
         # Classifier
         x = self.linear1(xs)
         x = self.bn1(x)
@@ -155,4 +91,3 @@ class GCNModel(Module):
         x = self.linear3(x)
 
         return x, sigmoid(x)
->>>>>>> e7ae4b2ce2c1b7b36249d5250c05ca5738212256
